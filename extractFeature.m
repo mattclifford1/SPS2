78563df3a868daf9d1aa9806d9b0339f80bdb4f0
mattclifford1 <mattc863@hotmail.com>
2017-04-06 18:28:24 +0100

    clean up2

--- extractFeature.m
+++ extractFeature.m
@@ -3,25 +3,12 @@ tic
 myFolder = '/Users/mattclifford/Documents/SPS/CourseWork2/characters/trainGIF';
 
 % myFolder = '/Users/mattclifford/Documents/SPS/CourseWork2/characters/trainGIF';
- myFolder = '/home/fe15/jc15311/linux/Documents/MATLAB/CW2/SPS2/trainGIF';
+%  myFolder = '/home/fe15/jc15311/linux/Documents/MATLAB/CW2/SPS2/trainGIF';
 timeElapsed = 0;
 filePattern = fullfile(myFolder, '*.GIF');
 gifFiles = dir(filePattern);
 feature1 = zeros(length(gifFiles),1);
 feature2 = zeros(length(gifFiles),1);
-for i = 1:length(gifFiles)
-  baseFileName = gifFiles(i).name;
-  fullFileName = fullfile(myFolder, baseFileName);
-  feature1(i) = getFeature1(fullFileName);
-  feature2(i) = getFeature2(fullFileName);
-end
-scatter(feature1(1:10),feature2(1:10))
-hold on
-scatter(feature1(11:20),feature2(11:20))
-scatter(feature1(21:30),feature2(21:30))
-xlabel('Feature 1'); ylabel('Feature 2'); 
-
-toc
 feature3 = zeros(length(gifFiles),1);
 for i = 1:length(gifFiles)
   fprintf('\n%d: \n',i)
--- getFeature1.m
+++ getFeature1.m
@@ -1,4 +1,4 @@
-function magQ = getFeature1(FILE)
+function mag = getFeature1(FILE)
 addpath(genpath('/Users/mattclifford/Documents/SPS/CourseWork2/characters/trainGIF'))
 if ischar(FILE) == 1
     F = imread(FILE);  %read in image
@@ -9,8 +9,6 @@ end
 Z = fft2(double(F));   
 % shift u=0,v=0 in the centre, **centre at 200, 320**
 Q = fftshift(Z);
-norm = sum(sum((abs(Q)).^2));
-
 maxQ = max(max(Q));
 
 if ischar(FILE) == 0
@@ -20,18 +18,12 @@ lower = 60; upper = 85;
 center = [0 0];    %u=0,v=0
 theta1 = lower*(pi/180); theta2 = upper*(pi/180);
 theta1_2 = -upper*(pi/180); theta2_2 = -lower*(pi/180);
-% Q1 = sectorFilter(Q,center,theta1,theta2,1000);
-% Q2 = sectorFilter(Q,center,theta1_2,theta2_2,1000);
 Q = sectorFilter2(Q,center,theta1,theta2,80);
-% Q = max(Q1,Q2);
-I = 35; J =70;     %width/height of box
-% Q = antiBoxFilter(Q,center,I,J);
+
 abQ = (abs(Q)).^2;
 mag = sum(sum(abQ));
-<<<<<<< HEAD
-mag = mag/norm;
 
-magQ = mag/maxQ;
+mag = mag/maxQ;
 imagesc(log(abs(Q)+1));colorbar;
 if ischar(FILE) == 0
     figure(1);subplot(2,2,3); imagesc(log(abs(Q)+1)); title('after filter 1'); colorbar;
