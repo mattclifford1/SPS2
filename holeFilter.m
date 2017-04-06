a4356e65b9ce2e897cbd694bad42176d345bea5c
mattclifford1 <mattc863@hotmail.com>
2017-04-06 15:45:05 +0100

    merge filter 3

--- holeFilter.m
+++ holeFilter.m
@@ -8,9 +8,10 @@ r1 = 0;
 for i = 1:dim(1)
     for j = 1:dim(2)
         u = i - center(1); v = j - center(2);
-        if u >= -r && u <= r && (v <= -sqrt(r1^2 - u^2) && v >= -sqrt(r^2 - u^2)) ...
-                || (v >= sqrt(r1^2 - u^2) && v <= sqrt(r^2 - u^2))
+        if u<= r && u>=-r && ( u >= -r && u <= r && (v <= -sqrt(r1^2 - u^2) && v >= -sqrt(r^2 - u^2)) ...
+                || (v >= sqrt(r1^2 - u^2) && v <= sqrt(r^2 - u^2)) )
             Q(i,j) = 0;
         end
     end
 end
+imagesc(log(abs(Q)+1));
